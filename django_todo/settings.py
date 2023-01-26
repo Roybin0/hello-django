14c2698fe29537c52333ce650a4406be310a3452
Robyn Smith <robyn.roybin@gmail.com>
2023-01-26 11:13:02 +0000

    Add host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -34,7 +34,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = ['todo-list-roybin.herokuapp.com']
 
 # Application definition
 
